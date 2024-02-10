06fd585f3436a083685e6714dc3253fba450b401
Ferdi Eraslan <frdiersln@hotmail.com>
2024-02-10 03:31:08 +0300

    +method added to fetch dollar exchange rate from www.tcmb.gov.tr

--- src/views/DolarBazliHesapView.vue
+++ src/views/DolarBazliHesapView.vue
@@ -1,15 +1,25 @@
 <script lang="ts">
 import CalcForm from '@/components/CalcForm.vue';
 
-import { defineComponent } from 'vue';
-export default defineComponent({
+export default {
     name: 'DolarBazliHesapView',
-    components: { CalcForm }
-});
+    components: { CalcForm },
+    methods: {
+		fetchData() {
+			fetch('https://api.allorigins.win/raw?url=https://www.tcmb.gov.tr/kurlar/202402/09022024.xml')
+      .then(response => response.text())
+      .then(data => {
+        console.log("1 Dolar " + data.split('ABD DOLARI')[1].split('<BanknoteSelling>')[1].split('</BanknoteSelling>')[0] + " TL");
+      })
+      .catch(error => console.error('Error:', error));
+		}
+	}
+};
 </script>
 
 <template>
   <div class="RightWrapper">
     <CalcForm formType="dolarBazli" />
+    <button @click="fetchData">Fetch Data</button>
   </div>
 </template>

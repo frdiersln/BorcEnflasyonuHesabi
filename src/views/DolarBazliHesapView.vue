f8001c6b4bb41df41a1bc050a2b7dfee9e10dd39
Ferdi Eraslan <frdiersln@hotmail.com>
2024-02-11 03:39:54 +0300

    ~v-if added to results

--- src/views/DolarBazliHesapView.vue
+++ src/views/DolarBazliHesapView.vue
@@ -20,8 +20,8 @@ export default {
 
 <template>
   <div class="RightWrapper">
-    <CalcForm v-if="results.length == 4" formType="dolarBazli" @passResults="receiveResultsFromCalcForm" />
-    <div  class="Results">
+    <CalcForm formType="dolarBazli" @passResults="receiveResultsFromCalcForm" />
+    <div v-if="results.length == 4" class="Results">
       <h2>Results</h2>
       <p>Alınan/Verilen Borç: {{ results[0] }}TL Geçmiş Dolar Kuru: {{results[1]}}TL Güncel Dolar Kuru: {{ results[2] }}TL Hesaplanan Borç: {{ results[3] }}TL </p>
     </div>

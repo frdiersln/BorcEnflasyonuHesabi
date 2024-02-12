5af50a5328123fb23a25ea25d7883cb23816a0b7
Ferdi Eraslan <frdiersln@hotmail.com>
2024-02-12 15:57:59 +0300

    ~results header is sticky now

--- src/views/DolarBazliHesapView.vue
+++ src/views/DolarBazliHesapView.vue
@@ -21,16 +21,18 @@ export default {
 <template>
   <div class="RightWrapper">
     <CalcForm formType="dolarBazli" @passResults="receiveResultsFromCalcForm" />
-    <div v-if="results.length > 0" class="results">
-      <div class="heading">
+    <div class="resultsWrapper">
+      <div v-if="results.length > 0" class="resultsHeading">
         <h2>Sonuçlar</h2>
       </div>
-      <div v-for="result in results" :key="result" class="row">
-        <div class="date"> {{ result[0] }} </div>
-        <div class="cell"><div class="label"> Borç Miktarı</div> <div class="value"> {{ result[1] }} </div></div>
-        <div class="cell"><div class="label"> Borç Tarihindeki Kur</div> <div class="value"> {{ result[2] }}</div></div>
-        <div class="cell"><div class="label"> Güncel Kur</div> <div class="value"> {{ result[3] }}</div></div>
-        <div class="cell"><div class="label"> Hesaplanan Borç</div> <div class="value"> {{ result[4] }}</div></div>
+      <div v-if="results.length > 0" class="results">
+        <div v-for="result in results" :key="result" class="row">
+          <div class="date"> {{ result[0] }} </div>
+          <div class="cell"><div class="label"> Borç Miktarı</div> <div class="value"> {{ result[1] }} </div></div>
+          <div class="cell"><div class="label"> Borç Tarihindeki Kur</div> <div class="value"> {{ result[2] }}</div></div>
+          <div class="cell"><div class="label"> Güncel Kur</div> <div class="value"> {{ result[3] }}</div></div>
+          <div class="cell"><div class="label"> Hesaplanan Borç</div> <div class="value"> {{ result[4] }}</div></div>
+        </div>
       </div>
     </div>
   </div>
